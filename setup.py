4f76aa1246bc65b7bde34f2314f5b264434fc50e
Cosmin Poieana <cmin764@gmail.com>
2018-04-24 15:49:12 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ ETC, RES = map(lambda arg: os.path.join(arg, "libmorse"), ["etc", "res"])
 
 setup(
     name="libmorse",
-    version="0.4.3",
+    version="0.4.4",
     description="Convert timed signals into alphabet.",
     long_description=read("README.md") or "",
     url="https://github.com/cmin764/libmorse",
